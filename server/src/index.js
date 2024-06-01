3eb521115a46d748251c85faba001c1349e06172
Gautam <gautamgohil0@gmail.com>
2024-06-02 03:04:53 +0530

    server token implementation updated

--- server/src/index.js
+++ server/src/index.js
@@ -5,41 +5,78 @@ import axios from "axios";
 
 const app = express();
 
-console.log(process.env.FRONTEND_URL);
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
   })
 );
 
-let token = "";
+let token = null;
+let lastTokenTimestamp = null;
+const speechKey = process.env.SPEECH_KEY;
+const speechRegion = process.env.SPEECH_REGION;
 
-app.get("/token", async (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  const speechKey = process.env.SPEECH_KEY;
-  const speechRegion = process.env.SPEECH_REGION;
+const getToken = async () => {
+  try {
+    const headers = {
+      headers: {
+        "Ocp-Apim-Subscription-Key": speechKey,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
 
-  const refreshTheToken = req.query?.refresh;
-
-  const headers = {
-    headers: {
-      "Ocp-Apim-Subscription-Key": speechKey,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  };
-
-  if (!token || refreshTheToken) {
     const tokenResponse = await axios.post(
       `https://${speechRegion}.api.cognitive.microsoft.com/sts/v1.0/issueToken`,
       null,
       headers
     );
+
     token = tokenResponse.data;
+    lastTokenTimestamp = Date.now();
+    console.log("Token refreshed at:", new Date(lastTokenTimestamp).toLocaleString());
+  } catch (error) {
+    console.error("Error while getting token:", error);
   }
-  res.send({
-    token: token,
-    region: speechRegion,
+};
+
+const scheduleTokenRefresh = () => {
+  setInterval(async () => {
+    try {
+      await getToken();
+    } catch (error) {
+      console.error("Error while scheduling token refresh:", error);
+    }
+  }, 9 * 60 * 1000); // 9 minutes in milliseconds
+};
+
+// Initial token generation
+getToken()
+  .then(() => {
+    scheduleTokenRefresh();
+  })
+  .catch((error) => {
+    console.error("Error while generating initial token:", error);
   });
+
+app.get("/token", async (req, res) => {
+  try {
+    res.setHeader("Content-Type", "application/json");
+
+    // When client asks for refresh token
+    const refreshTheToken = req.query?.refresh;
+
+    if (!token || refreshTheToken) {
+      await getToken();
+    }
+
+    res.send({
+      token: token,
+      region: speechRegion,
+    });
+  } catch (error) {
+    console.error("Error while handling /token request:", error);
+    res.status(500).send({ error: "An error occurred while processing your request." });
+  }
 });
 
 app.listen(8080, () => console.log("Server running on port 8080"));
